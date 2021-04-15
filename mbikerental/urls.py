a887b085f5c400be6b82902c52ace2ec1a8a15c5
bogdanpenciu <81584802+bogdanpenciu@users.noreply.github.com>
2021-04-15 20:42:22 +0300

    *

--- mbikerental/urls.py
+++ mbikerental/urls.py
@@ -33,8 +33,7 @@ urlpatterns = [
     path('social-auth/', include('social_django.urls', namespace="social")),
     ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
 
 
