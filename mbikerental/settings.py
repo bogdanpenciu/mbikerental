f72a6425cb12adc6f774a41e4a29341ad7e8f5ce
bogdanpenciu <81584802+bogdanpenciu@users.noreply.github.com>
2021-04-15 20:27:34 +0300

    added herku files

--- mbikerental/settings.py
+++ mbikerental/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,11 +82,9 @@ WSGI_APPLICATION = 'mbikerental.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
     }
-}
+DATABASES['default']=dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
