225f40f7e2773005c5fa8fe4000f8581f96cb148
bogdanpenciu <81584802+bogdanpenciu@users.noreply.github.com>
2021-04-16 10:11:09 +0300

    update settings

--- mbikerental/settings.py
+++ mbikerental/settings.py
@@ -130,7 +130,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-LOGIN_REDIRECT_URL ='/homepage'
+LOGIN_REDIRECT_URL ='/'
 LOGIN_URL = '/accounts/login'
 LOGOUT_REDIRECT_URL = LOGIN_URL
 
