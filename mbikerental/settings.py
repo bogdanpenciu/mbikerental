487e6c713ee0050345414ca173cbef4129d75878
bogdanpenciu <81584802+bogdanpenciu@users.noreply.github.com>
2021-05-31 22:15:16 +0300

    add Contact menu

--- mbikerental/settings.py
+++ mbikerental/settings.py
@@ -87,8 +87,8 @@ WSGI_APPLICATION = 'mbikerental.wsgi.application'
 # # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES ={}
-#     ={'default':
+DATABASES={}
+#     {'default':
 # {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',}
