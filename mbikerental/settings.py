3f786b336aab641bf5a78b986113bcc77d8b0bb1
bogdanpenciu <81584802+bogdanpenciu@users.noreply.github.com>
2021-06-01 22:07:19 +0300

    a

--- mbikerental/settings.py
+++ mbikerental/settings.py
@@ -87,13 +87,14 @@ WSGI_APPLICATION = 'mbikerental.wsgi.application'
 # # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES={'default':
-{
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',}
-
-    }
-# DATABASES['default']=dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES={}
+#     {'default':
+# {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',}
+#
+#     }
+DATABASES['default']=dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # # Password validation
